4a8c0e007e768aa7c804a3a8805bd90c45ee2c3a
carolyne <pcmaunda@gmail.com>
2022-08-24 13:56:11 +0300

    updated changes for js file

---
+++ assets/app.js
@@ -0,0 +1,19 @@
+let add = document.getElementById('increment');
+let remove = document.getElementById('decrement');
+
+let int = document.getElementById('number');
+let integer = 0;
+
+add.addEventListener('click', function(){
+    integer += 1;
+    int.innerHTML = integer;
+})
+
+remove.addEventListener('click', function(){
+    integer -= 1;
+    int.innerHTML = integer;
+})
+
+ 
+
+
